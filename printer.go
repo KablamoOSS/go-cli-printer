1fe3d81d6b1c96e1f8760d18729c52b0601684fb
Owen Kelly <owen@owenkelly.com.au>
2018-06-04 18:04:52 +1000

    updated docs and func name

--- printer.go
+++ printer.go
@@ -17,22 +17,20 @@ var once sync.Once
 func getPrinter() *spinner.Spinner {
 	once.Do(func() {
 		instantiated = spinner.New(spinner.CharSets[14], 100*time.Millisecond)
-
-		// instantiated.Writer = os.Stderr
 	})
 
 	return instantiated
 }
 
-// Print progress message
-func Print(message string) {
+//  Progress message with a spinner
+func Progress(message string) {
 	spinner := getPrinter()
 	spinner.Suffix = fmt.Sprintf("  %s", message)
 	spinner.Color("yellow")
 	spinner.Start()
 }
 
-// Step - Print a final step message to the console
+// Step prints a line console and stops the spinner
 func Step(message string) {
 	spinner := getPrinter()
 	spinner.FinalMSG = fmt.Sprintf("%s  %s \n", chalk.Yellow.Color(chalk.Dim.TextStyle("➜")), chalk.Bold.TextStyle(message))
@@ -41,7 +39,7 @@ func Step(message string) {
 	spinner.Stop()
 }
 
-// SubStep - Print a final step message to the console
+// SubStep prints a line console, at a given indent and stops the spinner
 func SubStep(message string, indent int, last bool) {
 	var indentString string
 
@@ -63,36 +61,56 @@ func SubStep(message string, indent int, last bool) {
 	}
 
 	spinner := getPrinter()
-	spinner.FinalMSG = fmt.Sprintf("%s%s %s \n", indentString, chalk.Dim.TextStyle(icon), chalk.Dim.TextStyle(message))
+	spinner.FinalMSG = fmt.Sprintf("%s%s %s \n", chalk.Dim.TextStyle(indentString), chalk.Dim.TextStyle(icon), chalk.Dim.TextStyle(message))
 	spinner.Start()
 	time.Sleep(2 * time.Second)
 	spinner.Stop()
 }
 
-// Finish - Print a final message to the console
+// Finish prints message to the console and stops the spinner with success.
+// This is best used to indicated the end of a task
 func Finish(message string) {
 	spinner := getPrinter()
-	spinner.FinalMSG = fmt.Sprintf("%s  %s \n", chalk.Green.Color("✔"), message)
+	spinner.FinalMSG = fmt.Sprintf("%s  %s \n", chalk.Green.Color("✔"), chalk.Bold.TextStyle(message))
 	spinner.Start()
 	time.Sleep(2 * time.Second)
 	spinner.Stop()
 }
 
-// Error - Finish with an error
-func Error(message string) {
+// Error prints an error to the screen. As it's intended reader is a user of your program,
+// it expects both the error message, a way for the reader to resolve the error, and if
+// possible a link to futher information.
+// If the error doesn't have a link, pass a blank string ""
+func Error(err error, resolution string, link string) {
 	spinner := getPrinter()
-	spinner.FinalMSG = fmt.Sprintf("%s  %s \n", chalk.Red.Color("✖"), message)
+
+	errMessage := fmt.Sprintf("%s  Error: %s \nHow to fix: %s \n", chalk.Red.Color("✖"), err, chalk.Dim.TextStyle(resolution))
+
+	if link != "" {
+		errMessage = fmt.Sprintf("%s\n More information: %s", errMessage, chalk.Dim.TextStyle(link))
+	}
+
+	spinner.FinalMSG = errMessage
 	spinner.Start()
 	time.Sleep(2 * time.Second)
 	spinner.Stop()
 }
 
-// Fatal - Panic with an error
-func Fatal(message string, err error) {
+// Fatal prints an error in the exact same way as Error, except it prefixes with "Fatal",
+// and the function ends with a panic
+func Fatal(err error, resolution string, link string) {
 	spinner := getPrinter()
-	spinner.FinalMSG = fmt.Sprintf("%s  %s \n", chalk.Red.Color("✖"), message)
+
+	errMessage := fmt.Sprintf("%s  Fatal: %s \nHow to fix: %s \n", chalk.Red.Color("✖"), err, chalk.Dim.TextStyle(resolution))
+
+	// Add the link if a valid one was supplied
+	if link != "" {
+		errMessage = fmt.Sprintf("%s\n More information: %s", errMessage, chalk.Dim.TextStyle(link))
+	}
+
+	spinner.FinalMSG = errMessage
 	spinner.Start()
 	time.Sleep(2 * time.Second)
 	spinner.Stop()
-	panic(err)
+	panic(1)
 }
