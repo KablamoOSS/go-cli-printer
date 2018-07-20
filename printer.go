2537c2fae18617496335cab6eb2e9050e03f452c
Owen Kelly <owen@owenkelly.com.au>
2018-07-20 14:59:49 +1000

    added capability to redirect output

--- printer.go
+++ printer.go
@@ -2,15 +2,15 @@ package printer
 
 import (
 	"fmt"
-	"os"
 	"io"
+	"log"
+	"os"
 	"sync"
 	"time"
-	"log"
 
-	"golang.org/x/crypto/ssh/terminal"
 	"github.com/briandowns/spinner"
 	"github.com/ttacon/chalk"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 var instantiated *spinner.Spinner
@@ -57,8 +57,15 @@ func Init(initVerbose bool, initColor string, initSpinner int, writer io.Writer)
 }
 
 // Test to set testing to true, to prevent exiting on Fatal errors
-func Test(){
+func Test() {
 	testing = true
+}
+
+// SetOutput to an io.Writer compatitble interface. Designed
+// to capture error messages for use in testing
+func SetOutput(out io.Writer) {
+	s := getPrinter()
+	s.Writer = out
 }
 
 // Create a singleton to the Spinner
@@ -75,27 +82,27 @@ func getPrinter() *spinner.Spinner {
 
 // Progress message with a spinner
 func Progress(message string) {
-	if(outputTTY){
-	spinner := getPrinter()
-	if message != previousProgressMessage {
-		previousProgressMessage = message
-		spinner.Suffix = fmt.Sprintf("  %s", message)
-		spinner.Color(color)
-	}
-	spinner.Start()
+	if outputTTY {
+		spinner := getPrinter()
+		if message != previousProgressMessage {
+			previousProgressMessage = message
+			spinner.Suffix = fmt.Sprintf("  %s", message)
+			spinner.Color(color)
+		}
+		spinner.Start()
 	}
 }
 
 // Step prints a line console and stops the spinner
 func Step(message string) {
-	if(outputTTY){
-	if message != previousStepMessage {
-		previousStepMessage = message
-		spinner := getPrinter()
+	if outputTTY {
+		if message != previousStepMessage {
+			previousStepMessage = message
+			spinner := getPrinter()
 
-		spinner.Stop()
-		fmt.Println(fmt.Sprintf("%s  %s", chalk.Yellow.Color("➜"), chalk.Bold.TextStyle(message)))
-	}
+			spinner.Stop()
+			fmt.Println(fmt.Sprintf("%s  %s", chalk.Yellow.Color("➜"), chalk.Bold.TextStyle(message)))
+		}
 	} else {
 		log.Print(message)
 	}
@@ -104,29 +111,29 @@ func Step(message string) {
 // SubStep prints a line console, at a given indent and stops the spinner
 // ignoreVerboseRule true, ensures the SubStep always prints
 func SubStep(message string, indent int, last bool, ignoreVerboseRule bool) {
-	if(outputTTY){
-	if message != previousSubStepMessage {
-		previousSubStepMessage = message
-		// Substeps are only printed if the verbose flag is set at init
-		// Unless it's the last substep
-		if verbose || ignoreVerboseRule || last {
-			var indentString string
+	if outputTTY {
+		if message != previousSubStepMessage {
+			previousSubStepMessage = message
+			// Substeps are only printed if the verbose flag is set at init
+			// Unless it's the last substep
+			if verbose || ignoreVerboseRule || last {
+				var indentString string
 
-			for i := 1; i <= indent; i++ {
-				indentString = fmt.Sprintf("   %s", indentString)
+				for i := 1; i <= indent; i++ {
+					indentString = fmt.Sprintf("   %s", indentString)
+				}
+
+				icon := "├─"
+
+				if last {
+					icon = "└─"
+				}
+				spinner := getPrinter()
+
+				spinner.Stop()
+				fmt.Println(fmt.Sprintf("%s%s %s", chalk.Dim.TextStyle(indentString), chalk.Dim.TextStyle(icon), chalk.Dim.TextStyle(message)))
 			}
-
-			icon := "├─"
-
-			if last {
-				icon = "└─"
-			}
-			spinner := getPrinter()
-
-			spinner.Stop()
-			fmt.Println(fmt.Sprintf("%s%s %s", chalk.Dim.TextStyle(indentString), chalk.Dim.TextStyle(icon), chalk.Dim.TextStyle(message)))
 		}
-	}
 	} else {
 		log.Print(message)
 	}
@@ -135,10 +142,10 @@ func SubStep(message string, indent int, last bool, ignoreVerboseRule bool) {
 // Finish prints message to the console and stops the spinner with success.
 // This is best used to indicated the end of a task
 func Finish(message string) {
-	if(outputTTY){
-	spinner := getPrinter()
-	spinner.Stop()
-	fmt.Println(fmt.Sprintf("%s  %s", chalk.Green.Color("✔"), chalk.Bold.TextStyle(message)))
+	if outputTTY {
+		spinner := getPrinter()
+		spinner.Stop()
+		fmt.Println(fmt.Sprintf("%s  %s", chalk.Green.Color("✔"), chalk.Bold.TextStyle(message)))
 	} else {
 		log.Printf("✔ %s", message)
 	}
@@ -146,74 +153,73 @@ func Finish(message string) {
 
 // Warn prints a warning to the screen. It's formatted like other errors, and coloured yellow.
 func Warn(err error, resolution string, link string) {
-	if(outputTTY){
-	spinner := getPrinter()
+	if outputTTY {
+		spinner := getPrinter()
 
-	spinner.Stop()
-	errMessage := fmt.Sprintf(
-		"%s %s",
-		chalk.Bold.TextStyle(chalk.Yellow.Color("!  Error:")),
-		chalk.Yellow.Color(err.Error()),
-	)
-	if resolution != "" {
-		errMessage = fmt.Sprintf(
-			"%s\n%s%s",
-			errMessage,
-			chalk.Dim.TextStyle(chalk.Bold.TextStyle("☞  Resolution: ")),
-			chalk.Dim.TextStyle(resolution),
+		spinner.Stop()
+		errMessage := fmt.Sprintf(
+			"%s %s",
+			chalk.Bold.TextStyle(chalk.Yellow.Color("!  Error:")),
+			chalk.Yellow.Color(err.Error()),
 		)
-	}
+		if resolution != "" {
+			errMessage = fmt.Sprintf(
+				"%s\n%s%s",
+				errMessage,
+				chalk.Dim.TextStyle(chalk.Bold.TextStyle("☞  Resolution: ")),
+				chalk.Dim.TextStyle(resolution),
+			)
+		}
 
-	if link != "" {
-		errMessage = fmt.Sprintf(
-			"%s\n%s%s",
-			errMessage,
-			chalk.Dim.TextStyle(chalk.Bold.TextStyle("∞  More info: ")),
-			chalk.Italic.TextStyle(link),
-		)
-	}
+		if link != "" {
+			errMessage = fmt.Sprintf(
+				"%s\n%s%s",
+				errMessage,
+				chalk.Dim.TextStyle(chalk.Bold.TextStyle("∞  More info: ")),
+				chalk.Italic.TextStyle(link),
+			)
+		}
 
-	fmt.Println(errMessage)
+		fmt.Println(errMessage)
 
 	} else {
 		log.Printf("WARN: %s", err.Error())
 	}
 }
 
-
 // Error prints an error to the screen. As it's intended reader is a user of your program,
 // it expects both the error message, a way for the reader to resolve the error, and if
 // possible a link to futher information.
 // If the error doesn't have a link, pass a blank string ""
 func Error(err error, resolution string, link string) {
-	if(outputTTY){
-	spinner := getPrinter()
+	if outputTTY {
+		spinner := getPrinter()
 
-	spinner.Stop()
-	errMessage := fmt.Sprintf(
-		"%s %s",
-		chalk.Bold.TextStyle(chalk.Red.Color("✖  Error:")),
-		chalk.Red.Color(err.Error()),
-	)
-	if resolution != "" {
-		errMessage = fmt.Sprintf(
-			"%s\n%s%s",
-			errMessage,
-			chalk.Dim.TextStyle(chalk.Bold.TextStyle("☞  Resolution: ")),
-			chalk.Dim.TextStyle(resolution),
+		spinner.Stop()
+		errMessage := fmt.Sprintf(
+			"%s %s",
+			chalk.Bold.TextStyle(chalk.Red.Color("✖  Error:")),
+			chalk.Red.Color(err.Error()),
 		)
-	}
+		if resolution != "" {
+			errMessage = fmt.Sprintf(
+				"%s\n%s%s",
+				errMessage,
+				chalk.Dim.TextStyle(chalk.Bold.TextStyle("☞  Resolution: ")),
+				chalk.Dim.TextStyle(resolution),
+			)
+		}
 
-	if link != "" {
-		errMessage = fmt.Sprintf(
-			"%s\n%s%s",
-			errMessage,
-			chalk.Dim.TextStyle(chalk.Bold.TextStyle("∞  More info: ")),
-			chalk.Italic.TextStyle(link),
-		)
-	}
+		if link != "" {
+			errMessage = fmt.Sprintf(
+				"%s\n%s%s",
+				errMessage,
+				chalk.Dim.TextStyle(chalk.Bold.TextStyle("∞  More info: ")),
+				chalk.Italic.TextStyle(link),
+			)
+		}
 
-	fmt.Println(errMessage)
+		fmt.Println(errMessage)
 
 	} else {
 		log.Printf("ERROR: %s", err.Error())
@@ -223,41 +229,41 @@ func Error(err error, resolution string, link string) {
 // Fatal prints an error in the exact same way as Error, except it prefixes with "Fatal",
 // and the function ends with a panic
 func Fatal(err error, resolution string, link string) {
-	if(outputTTY){
+	if outputTTY {
 		spinner := getPrinter()
 
-	errMessage := fmt.Sprintf(
-		"%s %s",
-		chalk.Bold.TextStyle(chalk.Red.Color("✖  Fatal:")),
-		chalk.Red.Color(err.Error()),
-	)
-
-	if resolution != "" {
-		errMessage = fmt.Sprintf(
-			"%s\n%s%s",
-			errMessage,
-			chalk.Dim.TextStyle(chalk.Bold.TextStyle("☞  Resolution: ")),
-			chalk.Dim.TextStyle(resolution),
+		errMessage := fmt.Sprintf(
+			"%s %s",
+			chalk.Bold.TextStyle(chalk.Red.Color("✖  Fatal:")),
+			chalk.Red.Color(err.Error()),
 		)
-	}
-	// Add the link if a valid one was supplied
-	if link != "" {
-		errMessage = fmt.Sprintf(
-			"%s\n%s%s",
-			errMessage,
-			chalk.Dim.TextStyle(chalk.Bold.TextStyle("∞  More info: ")),
-			chalk.Italic.TextStyle(link),
-		)
-	}
 
-	spinner.Stop()
-	fmt.Println(errMessage)
+		if resolution != "" {
+			errMessage = fmt.Sprintf(
+				"%s\n%s%s",
+				errMessage,
+				chalk.Dim.TextStyle(chalk.Bold.TextStyle("☞  Resolution: ")),
+				chalk.Dim.TextStyle(resolution),
+			)
+		}
+		// Add the link if a valid one was supplied
+		if link != "" {
+			errMessage = fmt.Sprintf(
+				"%s\n%s%s",
+				errMessage,
+				chalk.Dim.TextStyle(chalk.Bold.TextStyle("∞  More info: ")),
+				chalk.Italic.TextStyle(link),
+			)
+		}
+
+		spinner.Stop()
+		fmt.Println(errMessage)
 
 	} else {
 		log.Printf("FATAL: %s", err.Error())
 	}
 
-	if(testing == false){
+	if testing == false {
 		os.Exit(1)
 	}
 }
